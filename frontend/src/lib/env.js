951777a6b686f26d85b9f25229e02148c841f436
PhillY <philly706869@gmail.com>
2025-08-14 14:58:37 +0000

    frontend: 카테고리 SSR 예시

---
+++ frontend/src/lib/env.js
@@ -0,0 +1,1 @@
+export { CAJABAMBOO_API_URL as API_URL } from "$env/static/private";
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  const { data } = $props();
+  const { categories } = data;
+</script>
+
+{#each categories as category}
+  <button>{category.name}</button>
+{/each}
